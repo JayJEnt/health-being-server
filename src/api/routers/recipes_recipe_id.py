a9699b7a489eaa6f4a646475600c531593c49f4a
JayJCode <jayjcode@gmail.com>
2025-07-25 01:00:24 +0200

    restructured repo directories

--- src/api/routers/recipes_recipe_id.py
+++ src/api/routers/recipes_recipe_id.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 from typing import List
 
 from api.schemas.recipe import CreateRecipe, Recipe
-from db_conn import supabase_connection
+from database.supabase_connection import supabase_connection
 from config import settings
 
 
