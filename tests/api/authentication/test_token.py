8bb961d5b05f1565ef978ef901ead1e19fad3c18
JayJCode <jayjcode@gmail.com>
2025-08-26 23:57:06 +0200

    updated gitignore

--- tests/api/authentication/test_token.py
+++ tests/api/authentication/test_token.py
@@ -6,7 +6,7 @@ def test_create_access_token(mock_datetime_utcnow, user_data, expected_token):
     token = create_access_token(user_data)
     assert token == expected_token
 
-
+# TODO: continue
 # def test_validate_correct_token(mock_supabase, expected_token, user_data):
 #     user = asyncio.run(validate_token(expected_token))
 #     assert user.id == 420
